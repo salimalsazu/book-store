b26fd39d9f42f605a979936e404c48262cac7195
Salim Al Sazu <accsalimalsazu@gmail.com>
2023-09-03 22:16:15 +0600

    bugs fixed

--- src/app/modules/users/users.routes.ts
+++ src/app/modules/users/users.routes.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import { UserController } from './users.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
-router.get('/', UserController.getAllUsers);
-router.get('/:id', UserController.getAllUsersById);
-router.patch('/:id', UserController.updateUsers);
-router.delete('/:id', UserController.deleteUser);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
 
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsersById);
+
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.updateUsers);
+
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.deleteUser);
 
 export const UserRoutes = router;
