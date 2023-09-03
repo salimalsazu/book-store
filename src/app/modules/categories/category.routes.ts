f5a14c906beca95dd8707328c78e356c611a5861
Salim Al Sazu <accsalimalsazu@gmail.com>
2023-09-03 21:23:59 +0600

    routes dixed

--- src/app/modules/categories/category.routes.ts
+++ src/app/modules/categories/category.routes.ts
@@ -1,25 +1,22 @@
 import express from 'express';
 import { CategoryController } from './category.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
-router.post('/create-category', CategoryController.createCategory);
+router.post(
+  '/create-category',
+  auth(ENUM_USER_ROLE.ADMIN),
+  CategoryController.createCategory
+);
+
 router.get('/', CategoryController.getAllCategory);
+
 router.get('/:id', CategoryController.getAllCategoryById);
-router.patch('/:id', CategoryController.updateCategory);
-router.delete('/:id', CategoryController.deleteCategory);
 
-// router.patch(
-//   '/:id',
-//   validateRequest(AcademicDepartmentValidation.update),
-//   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-//   AcademicDepartmentController.updateOneInDB
-// );
 
-// router.delete(
-//   '/:id',
-//   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-//   AcademicDepartmentController.deleteByIdFromDB
-// );
+
+
 
 export const CategoryRoutes = router;
